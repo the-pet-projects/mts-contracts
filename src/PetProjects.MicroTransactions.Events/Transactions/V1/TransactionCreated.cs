7fd2f97d6858445b2d15a1a21d7485843ef6fb02
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-12-08 20:26:37 +0000

    add missing properties to transactioncreated event

--- src/PetProjects.MicroTransactions.Events/Transactions/V1/TransactionCreated.cs
+++ src/PetProjects.MicroTransactions.Events/Transactions/V1/TransactionCreated.cs
@@ -5,24 +5,34 @@
 
     public class TransactionCreated : TransactionEvent
     {
-        public TransactionCreated(Guid transactionId, Guid userId, DateTime dateTime)
+        public TransactionCreated(Guid transactionId, Guid itemId, int quantity, Guid userId, DateTime dateTime)
             : base(userId, dateTime)
         {
             this.TransactionId = transactionId;
+            this.ItemId = itemId;
+            this.Quantity = quantity;
         }
 
-        public TransactionCreated(Guid transactionId, Guid userId, Timestamp timestamp)
+        public TransactionCreated(Guid transactionId, Guid itemId, int quantity, Guid userId, Timestamp timestamp)
             : base(userId, timestamp)
         {
             this.TransactionId = transactionId;
+            this.ItemId = itemId;
+            this.Quantity = quantity;
         }
 
-        public TransactionCreated(Guid transactionId, Guid userId)
+        public TransactionCreated(Guid transactionId, Guid itemId, int quantity, Guid userId)
             : base(userId)
         {
             this.TransactionId = transactionId;
+            this.ItemId = itemId;
+            this.Quantity = quantity;
         }
 
         public Guid TransactionId { get; set; }
+
+        public Guid ItemId { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
