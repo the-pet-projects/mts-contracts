de7d6256f8e2a738126f8795eda08cb473964859
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-12-09 16:40:57 +0000

    add missing files

--- src/PetProjects.MicroTransactions.Commands/Transactions/V1/CreateTransaction.cs
+++ src/PetProjects.MicroTransactions.Commands/Transactions/V1/CreateTransaction.cs
@@ -4,14 +4,6 @@
 
     public class CreateTransaction : TransactionCommand
     {
-        public CreateTransaction(Guid transactionId, Guid itemId, int quantity, Guid userId) 
-            : base(userId)
-        {
-            this.TransactionId = transactionId;
-            this.ItemId = itemId;
-            this.Quantity = quantity;
-        }
-
         public Guid TransactionId { get; set; }
 
         public Guid ItemId { get; set; }
